4dfb4f0ec7b5667ea8e9ab4a3636f4a66369fef0
Ayan-css <ayan48311@gmail.com>
2025-05-07 20:54:20 +0530

    added a remove btn and localstorage

--- Ecommerce-Cart/script.js
+++ Ecommerce-Cart/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     { id: 3, name: "product3", price: 59.992 },
   ];
 
-  const cart = [];
+  const cart = loadCartFromStorage() ||[];
   const productList = document.getElementById("product-list");
   const cartItems = document.getElementById("cart-items");
   const emptyCartMessage = document.getElementById("empty-cart");
@@ -34,6 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   function addToCart(product) {
     cart.push(product);
+    saveCartToStorage()
     renderCart();
   }
   function renderCart() {
@@ -42,25 +43,46 @@ document.addEventListener("DOMContentLoaded", () => {
     if (cart.length > 0) {
       emptyCartMessage.classList.add("hidden");
       cartTotalMassege.classList.remove("hidden");
+      priceTotalDisplay.classList.remove("hidden");
       cart.forEach((item, index) => {
         totalPrice += item.price;
         const cartItem = document.createElement("div");
         cartItem.innerHTML = `
         ${item.name} - ${item.price.toFixed(2)}
+
+        <button class = "remove-btn" data-index = "${index}">Remove</button>
         `;
         cartItems.appendChild(cartItem);
-        priceTotalDisplay.textContent = `${totalPrice.toFixed(2)}`
       });
+      priceTotalDisplay.textContent = totalPrice.toFixed(2)
     } else {
       emptyCartMessage.classList.remove("hidden");
       cartTotalMassege.classList.add("hidden");
       priceTotalDisplay.classList.add("hidden");
     }
   }
+  cartItems.addEventListener('click',(e) => {
+    if (e.target.classList.contains("remove-btn")){
+     const index = parseInt(e.target.getAttribute("data-index"))
+      cart.splice(index, 1)
+      saveCartToStorage();
+      renderCart();
+    }
+  }
+  )
+  function saveCartToStorage() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  function loadCartFromStorage() {
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+  }
   checkoutBtn.addEventListener('click', () => {
     cart.length = 0;
     alert("checkOut succeccfully")
     renderCart()
+    saveCartToStorage()
   }
   )
 });
